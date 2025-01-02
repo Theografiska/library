b7f52d5d486b7d0373762a92489e5c5e17cc5711
Theodor Kaljo <theodor.kaljo@gmail.com>
2025-01-02 22:58:41 +0200

    Basic functionality of delete button (not working properly yet)

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ function addBookToLibrary(object) {
     myLibrary.push(object);
 }
 
-const bodyKeepsScore = new Book("The Body Keeps the Score", "Bessel van der Kolk", 450, "read");
-const kolmeKatkuVahel = new Book("Kolme katku vahel", "Jaan Kross", 700, "not read");
+const bodyKeepsScore = new Book("The Body Keeps the Score", "Bessel van der Kolk", 450, "Read");
+const thinkingFastSlow = new Book("Thinking, Fast and Slow", "Daniel Kahneman", 700, "Currently reading");
+const mansSearchForMeaning = new Book("Man's Search for Meaning", "Viktor Frankl", 242, "Read");
+const lawsOfHumanNature = new Book("The Laws of Human Nature", "Robert Greene", 640, "Read");
 
 /* TESTING */ 
 /*
@@ -25,7 +27,10 @@ console.log(kolmeKatkuVahel);
 */
 
 addBookToLibrary(bodyKeepsScore);
-addBookToLibrary(kolmeKatkuVahel);
+addBookToLibrary(thinkingFastSlow);
+addBookToLibrary(mansSearchForMeaning);
+addBookToLibrary(lawsOfHumanNature);
+
 
 console.log(myLibrary);
 
@@ -45,10 +50,76 @@ const loopThroughArray = array => {
             bookContent.classList.add("book-content");
             bookCard.appendChild(bookContent);
 
-            bookContent.textContent += `${value}`;
+            bookContent.textContent += value;
         })
 
+        /* functionality to delete a book */
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Remove book";
+        deleteButton.classList.add("delete-btn");
+        bookCard.appendChild(deleteButton);
+
+        deleteButton.addEventListener("click", () => {
+            bookCard.remove();
+        })
     }
 }
 
 loopThroughArray(myLibrary);
+
+/* clicking add new book brings out the form */
+const addBookButton = document.querySelector("#add-book-btn");
+const formSection = document.querySelector("#form-section");
+
+addBookButton.addEventListener("click", () => {
+    formSection.style.display = "block";
+
+    /* resetting data */
+    
+    let newTitle = document.querySelector("#new-title");
+    let newAuthor = document.querySelector("#new-author");
+    let newPages = document.querySelector("#new-pages");
+
+    newTitle.value = "";
+    newAuthor.value = "";
+    newPages.value = "";
+})
+
+/* clicking submit adds creates a new book card */
+const formSubmitbutton = document.querySelector("#form-submit");
+
+formSubmitbutton.addEventListener("click", () => {
+    let newTitle = document.querySelector("#new-title").value;
+    let newAuthor = document.querySelector("#new-author").value;
+    let newPages = document.querySelector("#new-pages").value;
+    let newRead = document.querySelector("#new-read").value;
+
+    const anotherBook = new Book(newTitle, newAuthor, newPages, newRead);
+    myLibrary.push(anotherBook);
+
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+        
+    cardContainer.appendChild(bookCard);
+
+    Object.values(anotherBook).forEach(value => {
+            let bookContent = document.createElement("p");
+            bookContent.textContent = "";
+            bookContent.classList.add("book-content");
+            bookCard.appendChild(bookContent);
+
+            bookContent.textContent += value;
+        })
+    
+    /* functionality to delete a book */
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Remove book";
+    deleteButton.classList.add("delete-btn");
+    bookCard.appendChild(deleteButton);
+    
+    console.log(myLibrary);
+
+    formSection.style.display = "none"; /* hiding the form again */
+})
+
+
