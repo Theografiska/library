c298d03470b375f0ab00713bc42d0bc66dfc5472
Theodor Kaljo <theodor.kaljo@gmail.com>
2025-01-03 10:21:49 +0200

    Improve functionality of deleting items

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, length, read) {
+function Book(title, author, length, read, index) {
 	this.title = title;
 	this.author = author;
 	this.length = length;
 	this.read = read;
+    this.index = index;
     /*
 	this.info = function() {
 		return `${this.title} by ${this.author}, ${this.length} pages, ${this.read}.`;
@@ -15,22 +16,23 @@ function addBookToLibrary(object) {
     myLibrary.push(object);
 }
 
-const bodyKeepsScore = new Book("The Body Keeps the Score", "Bessel van der Kolk", 450, "Read");
-const thinkingFastSlow = new Book("Thinking, Fast and Slow", "Daniel Kahneman", 700, "Currently reading");
-const mansSearchForMeaning = new Book("Man's Search for Meaning", "Viktor Frankl", 242, "Read");
-const lawsOfHumanNature = new Book("The Laws of Human Nature", "Robert Greene", 640, "Read");
+/* Adding initial books to library */ 
 
-/* TESTING */ 
-/*
-console.log(bodyKeepsScore);
-console.log(kolmeKatkuVahel);
-*/
+const getIndex = () => {
+    return myLibrary.length;
+}
 
+const bodyKeepsScore = new Book("The Body Keeps the Score", "Bessel van der Kolk", 450, "Read", getIndex());
 addBookToLibrary(bodyKeepsScore);
-addBookToLibrary(thinkingFastSlow);
-addBookToLibrary(mansSearchForMeaning);
-addBookToLibrary(lawsOfHumanNature);
 
+const thinkingFastSlow = new Book("Thinking, Fast and Slow", "Daniel Kahneman", 700, "Currently reading", getIndex());
+addBookToLibrary(thinkingFastSlow);
+
+const mansSearchForMeaning = new Book("Man's Search for Meaning", "Viktor Frankl", 242, "Read", getIndex());
+addBookToLibrary(mansSearchForMeaning);
+
+const lawsOfHumanNature = new Book("The Laws of Human Nature", "Robert Greene", 640, "Read", getIndex());
+addBookToLibrary(lawsOfHumanNature);
 
 console.log(myLibrary);
 
@@ -54,14 +56,26 @@ const loopThroughArray = array => {
         })
 
         /* functionality to delete a book */
+        
         let deleteButton = document.createElement("button");
         deleteButton.textContent = "Remove book";
         deleteButton.classList.add("delete-btn");
         bookCard.appendChild(deleteButton);
 
         deleteButton.addEventListener("click", () => {
+            /* removing item from the DOM */
             bookCard.remove();
+            /* removing item from the array */
+            myLibrary = myLibrary.filter((object) => {
+                return object.index !== i; 
+            })
+
+            /* updating indexes */
+            for (let j=0; j < myLibrary.length; j++) {
+                myLibrary[j].index = j;
+            }
         })
+        
     }
 }
 
@@ -79,10 +93,12 @@ addBookButton.addEventListener("click", () => {
     let newTitle = document.querySelector("#new-title");
     let newAuthor = document.querySelector("#new-author");
     let newPages = document.querySelector("#new-pages");
+    let newRead = document.querySelector("#new-read");
 
     newTitle.value = "";
     newAuthor.value = "";
     newPages.value = "";
+    newRead.value = "Read";
 })
 
 /* clicking submit adds creates a new book card */
@@ -93,8 +109,10 @@ formSubmitbutton.addEventListener("click", () => {
     let newAuthor = document.querySelector("#new-author").value;
     let newPages = document.querySelector("#new-pages").value;
     let newRead = document.querySelector("#new-read").value;
+    let newIndex = myLibrary.length;
+    console.log(newIndex);
 
-    const anotherBook = new Book(newTitle, newAuthor, newPages, newRead);
+    const anotherBook = new Book(newTitle, newAuthor, newPages, newRead, newIndex);
     myLibrary.push(anotherBook);
 
     let bookCard = document.createElement("div");
@@ -116,7 +134,20 @@ formSubmitbutton.addEventListener("click", () => {
     deleteButton.textContent = "Remove book";
     deleteButton.classList.add("delete-btn");
     bookCard.appendChild(deleteButton);
-    
+    deleteButton.addEventListener("click", () => {
+        /* removing item from the DOM */
+        bookCard.remove();
+        /* removing item from the array */
+        myLibrary = myLibrary.filter((object) => {
+            return object.index !== newIndex; 
+        })
+
+        /* updating indexes */
+        for (let j=0; j < myLibrary.length; j++) {
+            myLibrary[j].index = j;
+        }
+    })
+
     console.log(myLibrary);
 
     formSection.style.display = "none"; /* hiding the form again */
